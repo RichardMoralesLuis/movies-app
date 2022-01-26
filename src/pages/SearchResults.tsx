033f93472cae521ed8dbdc52e75257b3cad6e767
Richard Morales <richmorales@nologis.com>
2022-01-26 23:25:42 +0000

    add navbar and input search

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -5,7 +5,9 @@ import styled from '@emotion/styled';
 import { ResultsSections, SearchSections } from '../components/search/ResultsSections';
 import { Sections } from '../components/search/Sections';
 import { useSearchMovies } from '../hooks/search/useSearchMovies';
+import { Search } from '../components/filters/Search';
 import { useSearchCompanies } from '../hooks/search/useSearchCompanies';
+import { NavBar } from '../components/navbar/Navbar';
 
 const Container = styled.div`
   display: grid;
@@ -26,23 +28,29 @@ const SectionsName = styled.div`
 
 export const SearchResults: FC = () => {
   const { query }: any = useParams();
-  const { movies, totalMovies, isSearchingMovies, handleShowMoreFilms } = useSearchMovies(query);
-  const { casts, totalCasts, isSearchingCast, handleShowMoreCasts } = useSearchCasts(query);
-  const { companies, totalCompanies, isSearchingCompanies, handleShowMoreCompanies } = useSearchCompanies(query);
+  const [searchValue, setSearchValue] = useState(query);
   const [selectedSection, setSelectedSection] = useState<SearchSections>('Movies');
+  const { movies, totalMovies, isSearchingMovies, handleShowMoreFilms } = useSearchMovies(searchValue);
+  const { casts, totalCasts, isSearchingCast, handleShowMoreCasts } = useSearchCasts(searchValue);
+  const { companies, totalCompanies, isSearchingCompanies, handleShowMoreCompanies } = useSearchCompanies(searchValue);
 
   const handleSelect = (section: SearchSections) => setSelectedSection(section);
+  const handleSearch = (searchValue: string) => setSearchValue(searchValue);
 
   if (isSearchingMovies || isSearchingCast || isSearchingCompanies) {
     return <div>Searching...</div>;
   }
 
-  return <Container>
-    <SectionsName>
-      <ResultsSections totalMovies={totalMovies} totalCasts={totalCasts} totalCompanies={totalCompanies} onSelect={handleSelect} selectedSection={selectedSection}/>
-    </SectionsName>
-    <SectionContainer>
-      <Sections selectedSection={selectedSection} movies={movies} casts={casts} companies={companies}/>
-    </SectionContainer>
-  </Container>;
+  return <>
+    <NavBar/>
+    <Search onSearch={handleSearch} defaultValue={searchValue}/>
+    <Container>
+      <SectionsName>
+        <ResultsSections totalMovies={totalMovies} totalCasts={totalCasts} totalCompanies={totalCompanies} onSelect={handleSelect} selectedSection={selectedSection}/>
+      </SectionsName>
+      <SectionContainer>
+        <Sections selectedSection={selectedSection} movies={movies} casts={casts} companies={companies}/>
+      </SectionContainer>
+    </Container>
+  </>;
 };
