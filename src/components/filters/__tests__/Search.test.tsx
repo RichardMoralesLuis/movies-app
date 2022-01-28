dff4254b82e1bd914efc1b96b6ec37f21f374235
Richard Morales <richmorales@nologis.com>
2022-01-28 19:27:06 +0000

    search test

---
+++ src/components/filters/__tests__/Search.test.tsx
@@ -0,0 +1,44 @@
+import { fireEvent, render } from '@testing-library/react';
+import { Search } from '../Search';
+import userEvent from '@testing-library/user-event';
+
+const renderSearch = () => {
+  const searchSpy = jest.fn();
+  const utils = render(<Search onSearch={searchSpy}/>);
+
+  const getInput = () => utils.queryByPlaceholderText('Search your film');
+  const type = (text: string) => userEvent.type(getInput()!, text);
+  const pressEnter = () => fireEvent.keyPress(getInput()!, { key: 'Enter', code: 13, charCode: 13 });
+
+  return { utils, getInput, type, pressEnter, searchSpy };
+};
+describe('Search', function() {
+  describe('Search behaviour', function() {
+    it('should not call the onChange if the value is less than 3 letters', function() {
+      const { type, pressEnter, searchSpy } = renderSearch();
+
+      type('aa');
+      pressEnter();
+
+      expect(searchSpy).not.toBeCalled();
+    });
+
+    it('should call the onChange if the value is equal 3 letters', function() {
+      const { type, pressEnter, searchSpy } = renderSearch();
+
+      type('aaa');
+      pressEnter();
+
+      expect(searchSpy).toBeCalled();
+    });
+
+    it('should call the onChange if the value is more than 3 letters', function() {
+      const { type, pressEnter, searchSpy } = renderSearch();
+
+      type('aaaa');
+      pressEnter();
+
+      expect(searchSpy).toBeCalled();
+    });
+  });
+});
